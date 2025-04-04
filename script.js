0f2060c948cb9d71ffd530af4a0b841570a0f6e9
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-04-04 09:06:04 -0300

    Tentativa de realizar a questão 2 e começo da questão 3 com comentário inicial e de fim.

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 //Questão 1 - Somador de Números:
-
 let qtdValores = parseInt(prompt('Escreva quantos valores você deseja somar:'));
 let valorSoma = 0;
+let resultado = 0;
 
-function somarValores(resposta){
+function somarValores(qtdValores){
+for(i = 0; i < qtdValores; i++){
+    valorSoma = parseFloat(prompt("Escreva um valor para a soma"));
+    resultado = resultado + valorSoma;
+}
+console.log('O valor da soma de todos os valores é:' + resultado);
+}
+somarValores(qtdValores);
+//Encerramento da questão 1
+
+//Questão 2 - Contagem Regressiva da NASA:
+
+function contagemRegressiva(){
+
+}
 
 
+setInterval(contagemRegressiva, 1000);
+
+//Encerramento da questão 2
+
+//Questão 3 - Balanço Financeiro Anual:
 
 
-
-    }
+// Encerramento da questão 3 
