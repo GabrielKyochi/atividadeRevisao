b456d436dddd2f533e066c773b2b43157f4160b3
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-04-04 08:43:48 -0300

    Fiz o let qtdValores para definir quantos valores o usuário deseja somar e deixei o valorSoma = 0, e iniciei a função.

---
+++ script.js
@@ -0,0 +1,12 @@
+//Questão 1 - Somador de Números:
+
+let qtdValores = parseInt(prompt('Escreva quantos valores você deseja somar:'));
+let valorSoma = 0;
+
+function somarValores(resposta){
+
+
+
+
+
+    }
