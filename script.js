98624397a03389bc3c1e8593bac605166a4e2421
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-04-04 09:33:12 -0300

    Realização da questão 3, definindo todas as varíaveis como 0 no let. Depois usando um for para ficar realizando uma repetição de perguntas sobre o ganho bruto e o gasto até que o i chegue a 12. No final, usei um let saldofinal = saldototal - gasto total e um if para caso o saldofinal ser menor que 0, escrever que a empresa teve prezuízo e caso contrário, dizer que teve lucro.

--- script.js
+++ script.js
@@ -25,6 +25,21 @@ setInterval(contagemRegressiva, 1000);
 //Encerramento da questão 2
 
 //Questão 3 - Balanço Financeiro Anual:
+let ganhoBruto = 0;
+let gastoMensal = 0;
+let ganhoTotal = 0;
+let gastoTotal = 0;
 
-
+for(i = 0; i < 12; i++){
+    ganhoBruto = parseFloat(prompt('Escreva o valor bruto do mês ' + (i+1)));
+    ganhoTotal = ganhoTotal + ganhoBruto;
+    gastoMensal = parseFloat(prompt('Escreva o gasto do mês ' + (i+1)));
+    gastoTotal = gastoTotal + gastoMensal;
+}
+let saldoFinal = (ganhoTotal - gastoTotal);
+if(saldoFinal < 0){
+    console.log('A empresa teve prezuízo durante o período desse ano.');
+} else{
+    console.log('A empresa teve lucro durante o período desse ano.');
+}
 // Encerramento da questão 3 
